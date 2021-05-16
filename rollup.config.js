00186c2a2e96e3785388d8574def724b359e91c0
Osama Aamer <osama.aamer95@gmail.com>
2021-05-16 20:23:43 +0500

    Add build metrics

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,25 @@
 import json from "@rollup/plugin-json";
 import banner from "rollup-plugin-banner";
 const { preserveShebangs } = require("rollup-plugin-preserve-shebangs");
+import bundleSize from "rollup-plugin-bundle-size";
+import analyze from "rollup-plugin-analyzer";
+
+import { name, version } from "./package.json";
+
+import chalk from "chalk";
+import boxen from "boxen";
+
+const greeting = chalk.white.bold(`Building ${name}@v${version}`);
+
+const boxenOptions = {
+  padding: 1,
+  margin: 0.25,
+  borderStyle: "round",
+  borderColor: "cyan",
+};
+
+const buildHeader = boxen(greeting, boxenOptions);
+console.log(buildHeader);
 
 export default {
   input: "src/index.js",
@@ -12,5 +31,7 @@ export default {
     json(),
     banner("\nosamaaamer-cli v<%= pkg.version %>\n<%= pkg.author %>\n"),
     preserveShebangs(),
+    analyze(),
+    bundleSize(),
   ],
 };
